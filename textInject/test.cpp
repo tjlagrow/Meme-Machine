7741e4bd76a72471d602777663336bd0379b8141
Ian Garrett <igarrett@ix-trusty.cs.uoregon.edu>
2016-03-10 18:30:46 -0800

    Taking input and output paths along with a content string and properly meme-ing

--- textInject/test.cpp
+++ textInject/test.cpp
@@ -1,57 +1,73 @@
 #include "gd.h"
-#include "gdfontl.h"
+#include "gdfontg.h"
 #include <cstring>
 #include <cstdlib>
 #include <string>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int getexten(string path){
+    int result;
+    string exten = path.substr(path.find_last_of(".")+1);
+    
+    transform(exten.begin(),exten.end(), exten.begin(), ::tolower);
+
+    if(exten== "jpg"||exten=="jpeg"){
+        return 0;
+    } else if(exten=="png"){
+        return 1;
+    } else {return 2;}
+}
+
+string getFileName(const string& s){
+    char sep = '/';
+
+    #ifdef _WIN32
+       sep = '\\';
+    #endif
+
+       size_t i = s.rfind(sep, s.length());
+       if (i != string::npos) {
+          return(s.substr(i+1, s.length() - i));
+       }
+
+       return("");
+}
+// void generateMeme(string memeText, string inPath, string outPath){
+
+// }
 
 int main (int argc, char *argv[])
 {
-        // FILE *in;
-        // FILE *out;
-        // gdImagePtr im;
-        // int black;
-        // int white;
-
-        // /* String to draw. */
-        // // unsigned char *s = {'h','e','l','l','o',0};
-        // std::string s = "abc";
-
-        // in = fopen("mypicture.jpg", "rb");
-
-        // im = gdImageCreateFromJpeg(in);
-        // /* Background color (first allocated) */
-        // black = gdImageColorAllocate(im, 0, 0, 0);  
-        // /* Allocate the color white (red, green and blue all maximum). */
-        // white = gdImageColorAllocate(im, 255, 255, 255);  
-        // /* Draw a centered string. */
-        // gdImageString(im, gdFontGetLarge(), 0, 0, s, white);
-
-        // /* ... Do something with the image, such as 
-        //   saving it to a file... */
-        // out = fopen("gdfx.png", "wb");
-        // if (!out) {
-        //         fprintf(stderr, "Can't create gdfx.png\n");
-        //         return 1;
-        // }
-        // gdImagePng(im, out);
-        // fclose(out);
-
-
-        // /* Destroy it */
-        // gdImageDestroy(im);
 
         gdImagePtr im;
         FILE* out;
         FILE* in;
-        int black;
-        int white;
-        int len;
+        int black, white, len;
+
+        string testPath = "test/hello/bro/blaze/internetKid.jpg";
+        string inPath, outPath, fileName, fileExt, postMemeName;
+
+        inPath = "mypicture.jpg";
+        outPath = "testFilePath/";
+        fileName = getFileName(inPath);
+        fileExt = getexten(fileName);
+        // postMemeName =  fileName.insert(fileName.length()-4,"MEME");
+
+        // string testFinalName = outPath+postMemeName;
+        string testFinalName = outPath+inPath;
+        // if (getexten())
+
+        const char* stuff = testFinalName.c_str();
+
         /* String to draw. */
         in = fopen("mypicture.jpg", "rb");
-        std::string temp = "That look when you meming hard";
-        len = temp.length();
+        string memeText = "When you meme too hard but have no intention of stopping";
+        len = memeText.length();
         unsigned char *val=new unsigned char[len+1];
-        strcpy((char *)val,temp.c_str());
+        strcpy((char *)val,memeText.c_str());
 
         im = gdImageCreateFromJpeg(in);
         /* Background color (first allocated) */
@@ -59,13 +75,13 @@ int main (int argc, char *argv[])
         /* Allocate the color white (red, green and blue all maximum). */
         white = gdImageColorAllocate(im, 255, 255, 255);  
         /* Draw a centered string. */
-        gdImageString(im, gdFontGetLarge(),
-          im->sx / 2 - (len * gdFontGetLarge()->w / 2),
-          im->sy / 2 - gdFontGetLarge()->h / 2,
-          val, white);
-        out = fopen("gdfx.png", "wb");
+        gdImageString(im, gdFontGetGiant(),
+          im->sx / 2 - (len * gdFontGetGiant()->w / 2),
+          im->sy / 12 - gdFontGetGiant()->h / 2,
+          val, black);
+        out = fopen("dankShit.jpg", "wb");
         if (!out) {
-                fprintf(stderr, "Can't create gdfx.png\n");
+                fprintf(stderr, "Can't create dankShit.png\n");
                 return 1;
         }
         gdImagePng(im, out);
